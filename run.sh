cc5de3fe4e7f71209edfc5e770a480b2e3469780
Philipp Haußleiter <philipp@haussleiter.de>
2020-05-19 23:59:34 +0200

    updating run cmd

--- run.sh
+++ run.sh
@@ -2,6 +2,13 @@
 
 PORT=10101
 
+mkdir -p $PWD/.config/code-server
+
 echo "open http://127.0.0.1:$PORT"
 
-docker run -it -p $PORT:8080 -v "$PWD:/home/coder/project" phaus/code-server:latest
+docker run -it \
+    -p $PORT:8080 \
+    -v "$PWD/.config:/home/coder/.config" \
+    -v "$PWD:/home/coder/project" \
+    -u "$(id -u):$(id -g)" \
+    phaus/code-server:latest
