f179a298db3c7b35af16db182774f10473ec99d4
Philipp Haußleiter <philipp@haussleiter.de>
2020-05-20 00:07:40 +0200

    fixing config path

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ echo "open http://127.0.0.1:$PORT"
 
 docker run -it \
     -p $PORT:8080 \
-    -v "$PWD/.config:/home/coder/.config" \
+    -v "$PWD/.config:~/.config" \
     -v "$PWD:/home/coder/project" \
     -u "$(id -u):$(id -g)" \
     phaus/code-server:latest
