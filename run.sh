f07007cbdd0614c89dddc62212752884aa228b35
Philipp Haussleiter <philipp@haussleiter.de>
2020-10-08 09:15:42 +0200

    updating locale config

--- run.sh
+++ run.sh
@@ -2,13 +2,16 @@
 
 PORT=10101
 
-mkdir -p $PWD/.config/code-server
+mkdir -p    $PWD/projects \
+            $PWD/.config/code-server
 
 echo "open http://127.0.0.1:$PORT"
 
 docker run -it \
     -p $PORT:8080 \
     -v "$PWD/.config:/home/coder/.config" \
-    -v "$PWD:/home/coder/project" \
+    -v "$PWD/projects:/home/coder/project" \
+    -v "$HOME/.ssh:/home/coder/.ssh" \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     -u "$(id -u):$(id -g)" \
     phaus/code-server:latest
